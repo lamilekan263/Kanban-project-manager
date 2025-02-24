9eec313066c7c9a32528618a342b2d51d3f3a105
lamilekan263 <lamilekan263@gmail.com>
2025-02-24 19:38:05 +0100

    feat: redirects to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import LoginForm from "@/components/LoginForm";
-import LogoComponent from "@/components/Logo";
-import SignUp from "@/components/SignUpForm";
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className='h-screen overflow-hidden p-6 grid md:grid-cols-3'>
-      <div className="hidden md:block bg-primary  rounded-r-2xl">
-        <LogoComponent />
-      </div>
-      <div className="col-span-2 ">
-        <LogoComponent />
-        <div className='flex h-full items-center justify-center w-full'>
-          <SignUp />
-        </div>
-      </div>
-    </div>
-  );
+  redirect("/login");
 }
